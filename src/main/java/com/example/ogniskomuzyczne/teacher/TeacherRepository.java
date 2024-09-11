b41813a20834138ce6b795a2415f11710f29fbdd
Jason <extremewinds@gmail.com>
2024-09-11 19:43:26 +0200

    remove teacher package

--- src/main/java/com/example/ogniskomuzyczne/teacher/TeacherRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.ogniskomuzyczne.teacher;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-}
